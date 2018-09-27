3d2aca58173b9e6e60320942ba47b8bc21803245
Dirk <dneumann42@gmail.com>
2018-09-27 15:46:12 +0200

    basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'Rooms', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'Rooms', loadChildren: './rooms/rooms.module#RoomsPageModule' },
+  { path: 'Bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' },
+  { path: 'Settings', loadChildren: './settings/settings.module#SettingsPageModule' },
 ];
 
 @NgModule({
